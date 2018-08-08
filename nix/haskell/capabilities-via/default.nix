6351b97e57a3e366e5b07162a88d9eacb2e9deae
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-08-08 09:30:03 +0200

    Depend on lens

--- nix/haskell/capabilities-via/default.nix
+++ nix/haskell/capabilities-via/default.nix
@@ -1,12 +1,12 @@
-{ mkDerivation, base, deriving-compat, generic-lens, mtl, stdenv
-, transformers
+{ mkDerivation, base, deriving-compat, generic-lens, lens, mtl
+, stdenv, transformers
 }:
 mkDerivation {
   pname = "capabilities-via";
   version = "0.1.0.0";
   src = ../../..;
   libraryHaskellDepends = [
-    base deriving-compat generic-lens mtl transformers
+    base deriving-compat generic-lens lens mtl transformers
   ];
   doHaddock = false;
   homepage = "https://github.com/tweag/capabilities-via";
