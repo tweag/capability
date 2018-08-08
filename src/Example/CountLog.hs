f6047c1eda96250083f0a7b546aa7a81f85729ee
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-08-08 10:47:53 +0200

    Example program with capabilities

---
+++ src/Example/CountLog.hs
@@ -0,0 +1,49 @@
+module Example.CountLog where
+
+import Control.Monad.IO.Class
+import qualified Data.Char
+import Data.IORef
+
+
+class Monad m => Counter m where
+  count :: m Int
+
+class Monad m => Logger m where
+  logStr :: String -> m ()
+
+
+-- | Count twice.
+doubleCount :: Counter m => m Int
+doubleCount = count >> count
+
+-- | Log the given number.
+logNum :: Logger m => Int -> m ()
+logNum = logStr . ("num: " ++) . show
+
+-- | Double count and log the result, repeat once.
+program :: (Counter m, Logger m) => m ()
+program = do
+  doubleCount >>= logNum
+  doubleCount >>= logNum
+
+
+data CountCtx = CountCtx { counter :: IORef Int }
+
+data LogCtx m = LogCtx { logger :: String -> m () }
+
+data CountLogCtx m = CountLogCtx
+  { countCtx :: CountCtx
+  , logCtx :: LogCtx m
+  }
+
+
+mkCounter :: MonadIO m => m CountCtx
+mkCounter = do
+  ref <- liftIO $ newIORef 0
+  pure CountCtx { counter = ref }
+
+regularLogger :: MonadIO m => LogCtx m
+regularLogger = LogCtx { logger = liftIO . putStrLn }
+
+loudLogger :: MonadIO m => LogCtx m
+loudLogger = LogCtx { logger = liftIO . putStrLn . map Data.Char.toUpper }
