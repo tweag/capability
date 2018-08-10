36cde898b5ec9bf39a1ec1945459744615811880
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-08-10 15:28:24 +0200

    Add Coerce combinator

--- src/HasReader.hs
+++ src/HasReader.hs
@@ -9,6 +9,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
@@ -29,7 +30,7 @@ module HasReader
 import Control.Lens (over, view)
 import Control.Monad.IO.Class (MonadIO)
 import qualified Control.Monad.Reader.Class as Reader
-import Data.Coerce (coerce)
+import Data.Coerce (Coercible, coerce)
 import qualified Data.Generics.Product.Fields as Generic
 import GHC.Exts (Proxy#, proxy#)
 import GHC.Generics (Generic)
@@ -68,6 +69,20 @@ instance Reader.MonadReader r m => HasReader tag r (MonadReader m) where
   local_ _ = coerce @((r -> r) -> m a -> m a) Reader.local
   reader_ :: forall a. Proxy# tag -> (r -> a) -> MonadReader m a
   reader_ _ = coerce @((r -> a) -> m a) Reader.reader
+
+
+-- | Convert the environment using safe coercion.
+instance
+  ( Coercible from to, HasReader tag from m
+  , forall x y. Coercible x y => Coercible (m x) (m y) )
+  => HasReader tag to (Coerce to m)
+  where
+    ask_ tag = coerce @(m from) $ ask_ tag
+    local_
+      :: forall a. Proxy# tag -> (to -> to) -> Coerce to m a -> Coerce to m a
+    local_ tag = coerce @((from -> from) -> m a -> m a) $ local_ tag
+    reader_ :: forall a. Proxy# tag -> (to -> a) -> Coerce to m a
+    reader_ tag = coerce @((from -> a) -> m a) $ reader_ tag
 
 
 -- | Zoom in on the record field @field@ of type @r@ in the environment @r'@.
