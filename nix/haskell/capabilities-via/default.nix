a5360cb7db93be5b18e01f37fb6419b49d4eb233
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-08-13 15:11:10 +0200

    Derive HasReader from HasState

--- nix/haskell/capabilities-via/default.nix
+++ nix/haskell/capabilities-via/default.nix
@@ -1,14 +1,14 @@
-{ mkDerivation, base, containers, deriving-compat, generic-lens
-, lens, monad-control, monad-unlift, mtl, roles, stdenv, streaming
-, transformers
+{ mkDerivation, base, containers, deriving-compat, exceptions
+, generic-lens, lens, monad-control, monad-unlift, mtl, roles
+, stdenv, streaming, transformers
 }:
 mkDerivation {
   pname = "capabilities-via";
   version = "0.1.0.0";
   src = ../../..;
   libraryHaskellDepends = [
-    base containers deriving-compat generic-lens lens monad-control
-    monad-unlift mtl roles streaming transformers
+    base containers deriving-compat exceptions generic-lens lens
+    monad-control monad-unlift mtl roles streaming transformers
   ];
   homepage = "https://github.com/tweag/capabilities-via";
   license = stdenv.lib.licenses.bsd3;
