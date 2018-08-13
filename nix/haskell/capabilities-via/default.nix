555137eb15a14aad81ef21ac38b47504262a8b84
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-08-13 11:08:30 +0200

    HasWriter instance for Lift

--- nix/haskell/capabilities-via/default.nix
+++ nix/haskell/capabilities-via/default.nix
@@ -1,13 +1,14 @@
 { mkDerivation, base, deriving-compat, generic-lens, lens
-, monad-control, mtl, roles, stdenv, streaming, transformers
+, monad-control, monad-unlift, mtl, roles, stdenv, streaming
+, transformers
 }:
 mkDerivation {
   pname = "capabilities-via";
   version = "0.1.0.0";
   src = ../../..;
   libraryHaskellDepends = [
-    base deriving-compat generic-lens lens monad-control mtl roles
-    streaming transformers
+    base deriving-compat generic-lens lens monad-control monad-unlift
+    mtl roles streaming transformers
   ];
   homepage = "https://github.com/tweag/capabilities-via";
   license = stdenv.lib.licenses.bsd3;
