79250bb20f09c7cbd429978d90b9f7977b769149
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-08-15 17:31:19 +0200

    HasError example - interactive calculator

---
+++ examples/Error.hs
@@ -0,0 +1,130 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeInType #-}
+
+-- | Example uses and instances of the @HasError@ capability.
+module Error where
+
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import GHC.Generics (Generic)
+import Test.Hspec
+import Text.Read (readEither)
+
+import HasError
+
+import Test.Common
+
+
+----------------------------------------------------------------------
+-- Example Programs
+
+-- Calculator Example ------------------------------------------------
+
+data ParserError
+  = InvalidInput String
+  deriving (Show, Typeable)
+  deriving anyclass Exception
+
+parseNumber :: HasThrow "parser" ParserError m => String -> m Int
+parseNumber input = case readEither input of
+  Left err -> throw @"parser" $ InvalidInput err
+  Right num -> pure num
+
+
+data MathError
+  = NegativeInput
+  deriving (Show, Typeable)
+  deriving anyclass Exception
+
+sqrtNumber :: HasThrow "math" MathError m => Int -> m Int
+sqrtNumber num
+  | num < 0 = throw @"math" NegativeInput
+  | otherwise = pure $ round $ sqrt @Double $ fromIntegral num
+
+
+-- | Errors that can occur in the calculator application.
+data CalcError
+    -- | The parser component failed.
+  = ParserError ParserError
+    -- | The math component failed.
+  | MathError MathError
+  deriving (Generic, Show, Typeable)
+  deriving anyclass Exception
+
+-- | Calculator application
+--
+-- Prompts for positive numbers and prints their square roots.
+--
+-- The @HasCatch "calc"@ constraint is used to catch errors that can occur in
+-- any of the components: "parser", or "math".
+--
+-- The @HasThrow "parser"/"math"@ constraints are imposed by the components.
+--
+-- XXX: It might be preferable to have a combinator that eliminates the
+--   @HasThrow "parser"/"math"@ constraints by wrapping the corresponding
+--   exceptions with @ParserError/MathError@ and renaming the tags.
+--   @calculator@ would then have just one @HasThrow "calc"@ constraint.
+calculator ::
+  ( HasThrow "parser" ParserError m, HasThrow "math" MathError m
+  , HasCatch "calc" CalcError m, MonadIO m )
+  => m ()
+calculator = do
+  liftIO $ putStr "Enter positive number or 'Q' to quit\n> "
+  line <- liftIO getLine
+  case line of
+    "Q" -> pure ()
+    input -> do
+      catch @"calc"
+        do
+          num <- parseNumber input
+          root <- sqrtNumber num
+          liftIO $ putStrLn $ "sqrt = " ++ show root
+        \e -> liftIO $ putStrLn $ "Error: " ++ show e
+      calculator
+
+
+----------------------------------------------------------------------
+-- Instances
+
+-- | Deriving @HasThrow/HasCatch@ from @unliftio@.
+--
+-- @ParserError@s are wrapped as @CalcError@s using the @ParserError@
+-- constructor, and @MathError@s are wrapped as @CalcError@s using the
+-- @MathError@ constructor.
+newtype Calculator a = Calculator { runCalculator :: IO a }
+  deriving newtype (Functor, Applicative, Monad, MonadIO)
+  deriving (HasThrow "parser" ParserError) via
+    Ctor "ParserError" (MonadUnliftIO CalcError IO)
+  deriving (HasThrow "math" MathError) via
+    Ctor "MathError" (MonadUnliftIO CalcError IO)
+  deriving (HasCatch "calc" CalcError) via
+    MonadUnliftIO CalcError IO
+
+
+----------------------------------------------------------------------
+-- Test Cases
+
+spec :: Spec
+spec =
+  describe "Calculator" $
+    it "evaluates calculator" $ do
+      let input = "4\n-1\nxyz\nQ\n"
+          output =
+            "Enter positive number or 'Q' to quit\n\
+            \> sqrt = 2\n\
+            \Enter positive number or 'Q' to quit\n\
+            \> Error: MathError NegativeInput\n\
+            \Enter positive number or 'Q' to quit\n\
+            \> Error: ParserError (InvalidInput \"Prelude.read: no parse\")\n\
+            \Enter positive number or 'Q' to quit\n\
+            \> "
+      runCalculator calculator
+        `withInput` input
+        `shouldPrint` output
--- examples/Test.hs
+++ examples/Test.hs
@@ -5,6 +5,7 @@ module Main
 import Test.Hspec
 
 import qualified CountLog
+import qualified Error
 import qualified Reader
 import qualified State
 import qualified Stream
@@ -14,6 +15,7 @@ import qualified Writer
 main :: IO ()
 main = hspec $ do
   describe "CountLog" CountLog.spec
+  describe "Error" Error.spec
   describe "Reader" Reader.spec
   describe "State" State.spec
   describe "Stream" Stream.spec
