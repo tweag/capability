4c2adf1b26d97c298f8885863d4cfd4ed11df3d4
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-09-21 15:56:01 +0200

    Simplify example for blog-post

--- examples/WordCount.hs
+++ examples/WordCount.hs
@@ -92,9 +92,9 @@ countWordsAndLettersInText text =
 
 -- | Counter application context.
 data CounterCtx = CounterCtx
-  { letterCount :: IORef (Map Char Int)
+  { letterCount :: IORef (Occurrences Char)
     -- ^ Counting letter occurrences.
-  , wordCount :: IORef (Map Text Int)
+  , wordCount :: IORef (Occurrences Text)
     -- ^ Counting word occurrences.
   } deriving Generic
 
@@ -103,22 +103,21 @@ newtype Counter a = Counter { runCounter :: CounterCtx -> IO a }
   deriving (Functor, Applicative, Monad) via (ReaderT CounterCtx IO)
   deriving (HasWriter "letterCount" (Occurrences Char)) via
     (WriterLog  -- Generate HasWriter using HasState of Monoid
-    (Coerce (Occurrences Char)  -- Coerce Map to Occurrences
     (ReaderIORef  -- Generate HasState from HasReader of IORef
     (Field "letterCount" "ctx"  -- Focus on the field letterCount
     (MonadReader  -- Generate HasReader using mtl MonadReader
-    (ReaderT CounterCtx IO))))))  -- Use mtl ReaderT newtype
+    (ReaderT CounterCtx IO)))))  -- Use mtl ReaderT newtype
   deriving (HasWriter "wordCount" (Occurrences Text)) via
-    (WriterLog (Coerce (Occurrences Text) (ReaderIORef
-    (Field "wordCount" "ctx" (MonadReader (ReaderT CounterCtx IO))))))
+    WriterLog (ReaderIORef
+    (Field "wordCount" "ctx" (MonadReader (ReaderT CounterCtx IO))))
 
 
 -- | Given a text count the occurrences of all words and letters in it,
 -- excluding white space, and print the outcome to standard output.
 wordAndLetterCount :: Text -> IO ()
 wordAndLetterCount text = do
-  lettersRef <- newIORef Map.empty
-  wordsRef <- newIORef Map.empty
+  lettersRef <- newIORef $ Occurrences Map.empty
+  wordsRef <- newIORef $ Occurrences Map.empty
   let ctx = CounterCtx
         { letterCount = lettersRef
         , wordCount = wordsRef
@@ -128,7 +127,7 @@ wordAndLetterCount text = do
   runCounter counter ctx
   let printOccurrencesOf name ref = do
         putStrLn name
-        occurrences <- readIORef ref
+        Occurrences occurrences <- readIORef ref
         ifor_ occurrences $ \item num ->
           putStrLn $ show item ++ ": " ++ show num
   printOccurrencesOf "Letters" lettersRef
