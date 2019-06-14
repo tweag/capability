29837ac099e168bb710f9766339a759019bebafd
Gaël Deest <gael.deest@tweag.io>
2019-06-14 16:38:15 +0200

    Re-exporting 'Dict' from 'constraints' rather than re-defining it.

--- src/Capability/Constraints.hs
+++ src/Capability/Constraints.hs
@@ -14,14 +14,8 @@ module Capability.Constraints
   , Dict(..)
   ) where
 
+import Data.Constraint (Dict(..))
 import Data.Kind (Constraint)
-
--- | Evidence of a constraint that can be passed around as a value. Matching on
--- the constructor brings the constraint into scope.
---
--- This is the same as the @Dict@ datatype defined in the @constraints@ package,
--- but redefined here to avoid the dependency.
-data Dict (c :: Constraint) = c => Dict
 
 -- | Type family used used to apply a list of capabilities to a single type.
 --
